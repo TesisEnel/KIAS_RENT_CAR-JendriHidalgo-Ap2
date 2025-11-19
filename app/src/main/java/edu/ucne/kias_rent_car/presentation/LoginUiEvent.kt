b3c2fd3c64426a4f59934229b3633361dae88665
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-11-19 03:30:54 -0400

    Se agregó el evento del login.

---
+++ app/src/main/java/edu/ucne/kias_rent_car/presentation/LoginUiEvent.kt
@@ -0,0 +1,18 @@
+package edu.ucne.kias_rent_car.presentation
+
+sealed class LoginUiEvent {
+
+    data class UserNameChanged(val userName: String) : LoginUiEvent()
+
+    data class PasswordChanged(val password: String) : LoginUiEvent()
+
+    data object Login : LoginUiEvent()
+
+    data object RegisterClick : LoginUiEvent()
+
+    data object DismissRegisterDialog : LoginUiEvent()
+
+    data object TogglePasswordVisibility : LoginUiEvent()
+
+    data object ClearError : LoginUiEvent()
+}
