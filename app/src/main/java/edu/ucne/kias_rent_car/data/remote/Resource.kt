9787753ea1607654c7d3b5207fdd6b6f06ce6181
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-11-19 03:27:54 -0400

    Se agregó el Resource.

---
+++ app/src/main/java/edu/ucne/kias_rent_car/data/remote/Resource.kt
@@ -0,0 +1,10 @@
+package edu.ucne.kias_rent_car.data.remote
+
+sealed class Resource<out T> {
+
+    data class Success<T>(val data: T) : Resource<T>()
+
+    data class Error(val message: String) : Resource<Nothing>()
+
+    data object Loading : Resource<Nothing>()
+}
