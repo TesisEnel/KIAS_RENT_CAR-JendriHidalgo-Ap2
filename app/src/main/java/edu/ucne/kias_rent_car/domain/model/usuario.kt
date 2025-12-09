240508cedffa234753b410a845a375147d0241fe
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-12-08 21:28:06 -0400

    Se creó usuario que define estructura completa de usuarios y contiene lógica de negocio para validación.

--- app/src/main/java/edu/ucne/kias_rent_car/domain/model/usuario.kt
+++ app/src/main/java/edu/ucne/kias_rent_car/domain/model/usuario.kt
@@ -2,5 +2,11 @@ package edu.ucne.kias_rent_car.domain.model
 
 data class Usuario(
     val id: Int,
-    val userName: String
-)
+    val nombre: String,
+    val email: String,
+    val telefono: String?,
+    val rol: String
+) {
+    fun esAdmin(): Boolean = rol == "Admin"
+    fun esCliente(): Boolean = rol == "Cliente"
+}
