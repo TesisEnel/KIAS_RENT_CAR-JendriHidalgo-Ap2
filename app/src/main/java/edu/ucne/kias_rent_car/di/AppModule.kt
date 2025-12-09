8ea9f6211bc4ae71fc1693fac168244bf44490c9
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-12-08 21:14:17 -0400

    Se creó AppModule que provee instancia de AppDatabase con Room.databaseBuilder e inyecta todos los DAOs como singletons.

--- app/src/main/java/edu/ucne/kias_rent_car/di/AppModule.kt
+++ app/src/main/java/edu/ucne/kias_rent_car/di/AppModule.kt
@@ -7,9 +7,13 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import edu.ucne.kias_rent_car.data.dao.ReservationDao
-import edu.ucne.kias_rent_car.data.dao.VehicleDao
-import edu.ucne.kias_rent_car.data.database.AppDatabase
+import edu.ucne.kias_rent_car.data.local.dao.MensajeDao
+import edu.ucne.kias_rent_car.data.local.dao.ReservacionDao
+import edu.ucne.kias_rent_car.data.local.dao.ReservationConfigDao
+import edu.ucne.kias_rent_car.data.local.dao.UbicacionDao
+import edu.ucne.kias_rent_car.data.local.dao.UsuarioDao
+import edu.ucne.kias_rent_car.data.local.dao.VehicleDao
+import edu.ucne.kias_rent_car.data.local.database.AppDatabase
 import javax.inject.Singleton
 
 @Module
@@ -22,12 +26,20 @@ object AppModule {
             context,
             AppDatabase::class.java,
             "kia_rent_db"
-        ).build()
+        )
+            .fallbackToDestructiveMigration()
+            .build()
     }
-
+    @Provides
+    fun provideUsuarioDao(db: AppDatabase): UsuarioDao = db.usuarioDao()
     @Provides
     fun provideVehicleDao(db: AppDatabase): VehicleDao = db.vehicleDao()
-
     @Provides
-    fun provideReservationDao(db: AppDatabase): ReservationDao = db.reservationDao()
+    fun provideUbicacionDao(db: AppDatabase): UbicacionDao = db.ubicacionDao()
+    @Provides
+    fun provideReservacionDao(db: AppDatabase): ReservacionDao = db.reservacionDao()
+    @Provides
+    fun provideMensajeDao(db: AppDatabase): MensajeDao = db.mensajeDao()
+    @Provides
+    fun provideReservationConfigDao(db: AppDatabase): ReservationConfigDao = db.reservationConfigDao()
 }
