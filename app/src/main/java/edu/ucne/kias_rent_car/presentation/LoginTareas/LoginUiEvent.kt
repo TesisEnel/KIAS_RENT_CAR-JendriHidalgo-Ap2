d03d312c47f8e422fbe1ad5065765309a4728974
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-12-10 03:46:34 -0400

    UiEvent para el manejo de eventos en las pantallas.

--- app/src/main/java/edu/ucne/kias_rent_car/presentation/LoginTareas/LoginUiEvent.kt
+++ app/src/main/java/edu/ucne/kias_rent_car/presentation/LoginTareas/LoginUiEvent.kt
@@ -1,18 +1,9 @@
 package edu.ucne.kias_rent_car.presentation.LoginTareas
 
 sealed class LoginUiEvent {
-
-    data class UserNameChanged(val userName: String) : LoginUiEvent()
-
+    data class EmailChanged(val email: String) : LoginUiEvent()  // ← Debe ser 'EmailChanged'
     data class PasswordChanged(val password: String) : LoginUiEvent()
-
     data object Login : LoginUiEvent()
-
-    data object RegisterClick : LoginUiEvent()
-
-    data object DismissRegisterDialog : LoginUiEvent()
-
     data object TogglePasswordVisibility : LoginUiEvent()
-
     data object ClearError : LoginUiEvent()
 }
